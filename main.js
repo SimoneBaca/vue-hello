1d5d57fc2f9d4cf649c212452ce8960950b539b7
Simonebaca <simobaco99@gmail.com>
2023-03-28 17:11:52 +0200

    error

---
+++ main.js
@@ -0,0 +1,12 @@
+/*Descrizione:
+Stampare a schermo un messaggio all’interno di un :puntare_a_destra: h1 :puntare_a_sinistra: utilizzando una proprietá nell'oggetto restituito dalla funzione data.*/
+
+const app = new Vue(
+    {
+        el: '#container',
+        data: {
+            message: 'Ciao Belli',
+            image:""
+        }
+    }
+);
