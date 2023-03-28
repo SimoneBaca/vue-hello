be1b5e0b00f1b131070ddda4b158dbb7522f4f97
Simonebaca <simobaco99@gmail.com>
2023-03-28 19:01:43 +0200

    end ex plus bonus

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const { createApp } = Vue;
   createApp({
     data() {
       return {
-        message: "Ciao belli",
+        message: "Ciao Belli",
+        imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQw4PpmJjh2Ym8rqRm3tPgifO_oNVf7IPi176_jer6cQ&s"
       }
     }
   }).mount('#app');
